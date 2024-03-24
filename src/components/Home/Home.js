d7f08bf150bdaf3635b31e3609a19c7c96e6ca66
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-03-24 21:38:41 +0500

    updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,50 +1,95 @@
-import "./Home.css"
+import "./Home.css";
 import Typewriter from "typewriter-effect";
-import { Email, Phone } from '@material-ui/icons'
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import { Email, Phone } from "@material-ui/icons";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
-function Home()
-{
+function Home() {
+  return (
+    <section className="section home center">
+      <h1>
+        Hi, I am <span className="about__name">Sana Ullah Rasheed</span>
+      </h1>
 
-    return(
+      <h2 className="typer">
+        <Typewriter
+          options={{
+            strings: [
+              "A Full Stack Web Developer",
+              "Front End Web Developer",
+              "Back End Developer",
+              "Certified from LinkedIn",
+              "Member of Google Developer Community",
+              "Member of MLSA (Microsoft Learn Student Ambassador)",
+            ],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 50,
+          }}
+        />
+      </h2>
 
-        <section className="section home center">
-         <h1>
-          Hi, I am <span className='about__name'>Sana Ullah Rasheed</span>
-        </h1>
-    
-      <h2 className="typer"> 
-        <Typewriter options={{strings:["A Full Stack Web Developer", "Front End Web Developer","Back End Developer","Certified from LinkedIn","Member of Google Developer Community","Member of MLSA (Microsoft Learn Student Ambassador)"],
-        autoStart:true,loop:true,deleteSpeed:50}}/>
-      </h2> 
+      <p id="des" style={{ textAlign: "justify" }}>
+        Versatile and conscientious web developer proficient in crafting
+        production-ready code utilizing HTML, CSS, JavaScript, and React.js for
+        front-end development. Experienced in utilizing Node.js, Express.js, and
+        MongoDB on the back-end to construct efficient single-page applications.
+      </p>
 
+      <section id="social">
+        <a
+          href="https://github.com/SanaUllahRasheed123"
+          target="_blank"
+          aria-label="github"
+          className="link link--icon"
+        >
+          {" "}
+          <GitHubIcon />{" "}
+        </a>
 
-        <p id="des" style={{ textAlign: 'justify' }}>
+        <a
+          href="https://www.linkedin.com/in/sana-ullah-5a3844264"
+          target="_blank"
+          aria-label="linkedin"
+          className="link link--icon"
+        >
+          {" "}
+          <LinkedInIcon />{" "}
+        </a>
 
-        Versatile and conscientious web developer proficient in crafting production-ready code utilizing HTML, CSS, JavaScript, and React.js for front-end development. Experienced in utilizing Node.js, Express.js, and MongoDB on the back-end to construct efficient single-page applications. 
-         </p>
-  
- 
-       <section id="social">
-         <a href="https://github.com/SanaUllahRasheed123" target="_blank" aria-label='github' className='link link--icon'> <GitHubIcon /> </a>
-         
-         <a href="https://www.linkedin.com/in/sana-ullah-5a3844264" target="_blank" aria-label='linkedin' className='link link--icon'> <LinkedInIcon /> </a>
-         
-         <a href='mailto:sanaullahrasheed3@gmail.com' aria-label="email" className='link link--icon'> <Email/> </a>
+        <a
+          href="mailto:sanaullahrasheed3@gmail.com"
+          aria-label="email"
+          className="link link--icon"
+        >
+          {" "}
+          <Email />{" "}
+        </a>
 
-         <a href='tel:+923099113463' aria-label="phone" className='link link--icon'> <Phone/> </a>
-    
+        <a
+          href="tel:+923099113463"
+          aria-label="phone"
+          className="link link--icon"
+        >
+          {" "}
+          <Phone />{" "}
+        </a>
       </section>
-     
-   
-         <a href="https://drive.google.com/file/d/16qZVUCvLrhY41CB6882BOke72LJVcm_a/view?usp=drive_link" target="_blank" id="resumeButton">
-            <span type='button' className='btn btn--outline'>  Resume </span>
-          </a>
-   
-    </section>)
+
+      <a
+        href="https://drive.google.com/file/d/1LJ0SOAwSDCS0tT9F7v5wCButNo0XUknW/view?usp=sharing"
+        target="_blank"
+        id="resumeButton"
+      >
+        <span type="button" className="btn btn--outline">
+          {" "}
+          Resume{" "}
+        </span>
+      </a>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
 
 //added
